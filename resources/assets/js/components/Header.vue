c9d599c23102ffe665cafc29c21579a63be484b8
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-04-23 11:27:57 -0400

    Setting up the application

---
+++ resources/assets/js/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+	<header>
+
+		<nav class="navbar bg-dark navbar-dark">
+		  <div class="container">
+		  	
+			<!-- Brand/logo -->
+			<a class="navbar-brand" href="#">Peluqueria</a>
+
+			<!-- Links -->
+			<ul class="navbar-nav">
+			<li class="nav-item">
+			  <a class="nav-link" href="#">Login</a>
+			</li>
+			</ul>
+		  </div>
+		</nav>
+	</header>
+</template>
---
+++ resources/assets/js/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Example Component</div>
+
+                <div class="card-body">
+                    {{ welcome }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        name: 'home',
+        computed: {
+            welcome() {
+                return this.$store.getters.welcome
+            }
+        }
+    }
+</script>
