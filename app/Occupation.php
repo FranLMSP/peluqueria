1f372ab935ffe2f6df5d45b37e5aec07d7b27127
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-05-01 07:32:11 -0400

    Creating the migrations and seeders for occupations

---
+++ app/Occupation.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Occupation extends Model
+{
+    protected $fillable = [
+    	'name', 'description'
+    ];
+
+    public $timestamps = false;
+}
---
+++ database/seeds/OccupationsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Occupation;
+
+class OccupationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Occupation::truncate();
+
+        factory(Occupation::class)->create([
+        	'name' => 'Peluquero',
+        	'description' => 'Encargado de hacer cortes de cabello a los clientes'
+        ]);
+
+        factory(Occupation::class)->create([
+        	'name' => 'Administrador',
+        	'description' => 'Encargado de llevar las cuentas'
+        ]);
+
+        factory(Occupation::class)->create([
+        	'name' => 'Cajero',
+        	'description' => 'Encargado de cobrarle a los clientes'
+        ]);
+    }
+}
